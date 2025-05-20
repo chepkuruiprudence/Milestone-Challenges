354aedb7f6d33b72132f7ef3acfd7f249251ac2d
chepkuruiprudence <chepkuruiprudence403@gmail.com>
2025-05-20 23:48:09 +0300

    creates a function that generates greetings depending on time

--- milestone_1_challenges.js
+++ milestone_1_challenges.js
@@ -63,7 +63,7 @@ findLargest(5, 9, 3);
 findLargest(10, 10, 10); 
 findLargest(-1, -5, -3); 
 
-// Challenge 6: calculate BMI weigh
+// Challenge 6: calculate BMI weight
 function calculateBMI(weight,height){
  BMI = weight / (height * height)
  if  (BMI < 18.5){
@@ -80,4 +80,21 @@ function calculateBMI(weight,height){
 calculateBMI(68, 1.75);
 calculateBMI(85, 1.8);
 
+// Challenge 8: calculate BMI weight
+function greetUser(name, hour){
+ if (hour>5 && hour<11){
+  console.log("Good morning," + name)
+ } else if(hour>12 && hour<17){
+  console.log("Good afternoon," + name)
+ }else if(hour>18 && hour<21){
+  console.log("Good evening," + name)
+ }else{
+  console.log("Good night, " + name)
+ }
+}
+
+greetUser("Prudence", 9)
+greetUser("Prudence", 14)
+greetUser("Prudence", 19)
+greetUser("Prudence", 23)
 
