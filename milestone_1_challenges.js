956565cbb9f7ef522d2a66d9d03c342584926cb8
chepkuruiprudence <chepkuruiprudence403@gmail.com>
2025-05-20 16:15:21 +0300

    create a function that converts minutes to seconds
    updates the name of the addition function

--- milestone_1_challenges.js
+++ milestone_1_challenges.js
@@ -1,8 +1,15 @@
 // Challenge 1: Sum of two numbers
-function addNum(a,b){
+function addition(a,b){
  console.log(a+b);
 }
 
-addNum(5,3)
-addNum(-6, 9)
+addition(5,3)
+addition(-6, 9)
 
+// Challenge 2: Sum of two numbers
+function convert(minutes){
+ console.log(minutes*60) ;
+}
+
+convert(5);
+convert(2);
