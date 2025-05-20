91c29acffd5bc131996ac9660da7402676d92136
chepkuruiprudence <chepkuruiprudence403@gmail.com>
2025-05-20 16:23:07 +0300

    creates a perimeter calculator of a rectangle

--- milestone_1_challenges.js
+++ milestone_1_challenges.js
@@ -6,10 +6,18 @@ function addition(a,b){
 addition(5,3)
 addition(-6, 9)
 
-// Challenge 2: Sum of two numbers
+// Challenge 2: convert minutes to seconds
 function convert(minutes){
  console.log(minutes*60) ;
 }
 
 convert(5);
 convert(2);
+
+// Challenge 3: calculate perimeter of a rectangle
+function findPerimeter(L,W){
+ console.log(L*W)
+}
+
+findPerimeter(6, 7)
+findPerimeter(20,10)
