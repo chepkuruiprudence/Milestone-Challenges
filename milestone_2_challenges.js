52d60839b7f50d318e7e23900c87f3fed9502776
chepkuruiprudence <chepkuruiprudence403@gmail.com>
2025-05-22 22:09:01 +0300

    a function that adds even numbers in an array

--- milestone_2_challenges.js
+++ milestone_2_challenges.js
@@ -63,10 +63,33 @@ return longest;
 }
 console.log(findLongestWord(["apple", "banana", "pear", "grapefruit"]));
 
-// Challenge 5:longest word
+// Challenge 5:count properties
 
 function countProperties(obj){
 return Object.keys(obj).length;
 }
 
 console.log(countProperties({ name: "Alice", age: 25, city: "Paris" }));
+
+// Challenge 6:filter by length
+function filterByLength(strings, minLength){
+ return strings.filter(function(str){
+  return str.length >= minLength;
+ })
+}
+console.log(filterByLength(["cat", "giraffe", "hippo", "dog", "elephant"], 5));
+
+// Challenge 7 :Sum of Even Numbers
+
+function sumEvenNumbers(arr){
+  let sum=0;
+  
+  for (let i=0; i<arr.length; i++){
+    if (arr[i]%2 ==0){
+      sum+=arr[i];
+    }
+  }
+ return sum;
+}
+
+console.log (sumEvenNumbers([1, 2, 3, 4, 5, 6]));
