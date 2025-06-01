70b70b5bf151d7bec7ef77e654456f241e712595
chepkuruiprudence <chepkuruiprudence403@gmail.com>
2025-06-01 19:23:11 +0300

    creates a function that takes minutes and convert to seconds

--- src/milestone_1_challenges.ts
+++ src/milestone_1_challenges.ts
@@ -4,3 +4,10 @@ function addition(a : number, b : number){
 }
 addition(5,3)
 addition(-5,9)
+
+//Challenge2: Convert Minutes into seconds
+function convert(minutes: number){
+  console.log(minutes*60)
+}
+convert(5);
+convert(2);
