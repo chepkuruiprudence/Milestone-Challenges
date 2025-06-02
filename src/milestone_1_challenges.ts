95a29baf41b88cc35b83f90f9396e09238827f1f
chepkuruiprudence <chepkuruiprudence403@gmail.com>
2025-06-02 11:16:10 +0300

    Creates a funtion that multiplies a number by itself

--- src/milestone_1_challenges.ts
+++ src/milestone_1_challenges.ts
@@ -143,6 +143,20 @@ function sumEvenNumbers(n : number) : void{
  sumEvenNumbers(6); 
  sumEvenNumbers(10);
  sumEvenNumbers(5);
+
+ // Challenge 12: Multiply by itself
+function powerUp(num : number, times : number): number{
+  let result=1;
+  for ( let i=0; i<times; i++){
+   result *=num;
+  }
+  return result;
+ }
+ 
+ console.log(powerUp(2, 3)); 
+ console.log(powerUp(5, 0));
+ console.log(powerUp(3, 4)); 
+ console.log(powerUp(0, 0));
  
  
  
